4d2681257fa137d80957041a4906c307c17cbc94
André Vidas Olsen <gdn417@sund33194.sund.root.ku.dk>
2018-05-23 14:14:43 +0200

    First commit

---
+++ R/GOenrichment.R
@@ -0,0 +1,18 @@
+rm(list=ls())
+library(org.Mm.eg.db) # Genome wide annotation for Mouse, primarily based on mapping using Entrez Gene identifiers. ref: https://bioconductor.org/packages/release/data/annotation/html/org.Mm.eg.db.html
+library(topGO)
+
+# load all genes from DESeq2 result data
+geneList <- read.csv(file = "../../../projects/nfd/results/DE_genes/MllAF9_vs_NF.csv", row.names = 1, as.is = TRUE)
+dim(geneList)
+
+
+# convert geneList to topGO input format
+geneUni <- geneList$padj
+names(geneUni) <- rownames(geneList) 
+
+
+# GO enrichment analysis
+GOdata <- new("topGOdata", ontology = "BP", allGenes = geneUni, geneSel = function(p) p < 0.01, description = "Test", annot = annFUN.org, mapping = "org.Mm.eg.db", ID = "Ensembl")
+resultFisher <- runTest(GOdata, algorithm = "classic", statistic = "fisher")
+GenTable(GOdata, classicFisher = resultFisher, topNodes = 10)
