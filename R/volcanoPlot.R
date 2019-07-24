48ee0f7b9755addec62d2112ff569877055f1114
André Vidas Olsen <andre.v.olsen@gmail.com>
2019-07-24 15:28:15 +0200

    include remove NAs from padj values + generalization of top_up_down

--- R/volcanoPlot.R
+++ R/volcanoPlot.R
@@ -4,15 +4,23 @@ library(ggplot2)
 library(ggrepel)
 
 #### load data
-genes <- read.csv(file = "dataChunqin/results/DE_gene_list/exp.csv", row.names = 1)
-genes$Gene <- rownames(genes) 
+#genes <- read.csv2(file = "../results/DEgenes/codingGenes/postHocFiltering/trt_vs_ctrl.csv", row.names = 1)
+#genes <- read.csv2(file = "../results/DEgenes/codingGenes/postHocFiltering/relap_vs_trt.csv", row.names = 1)
+genes <- read.csv2(file = "../results/DEgenes/codingGenes/postHocFiltering/relap_vs_ctrl.csv", row.names = 1)
+
+#genes$Gene <- rownames(genes) 
 head(genes)
 alphaLevel <- 0.05 
 geneNamesThres <- 1e-100
 logFC_thres <- 1 # when changing this, remember to change the FC legends in the plot
+top_up_down <- 10
 pAdjMethod <- "FDR"
 
 
+# remove na
+dim(genes)
+genes <- genes[!is.na(genes$padj),]
+dim(genes)
 
 genes$Significance <- NA
 genes[genes$padj > alphaLevel, "Significance"] <- "Not signicant"
@@ -23,9 +31,12 @@ dim(genes)
 
 
 # find 10 most up and down regulated genes
-up_top10 <- head(genes[order(-genes$log2FoldChange),], n = 10)
-down_top10 <- head(genes[order(genes$log2FoldChange),], n = 10)
+up_top <- head(genes[order(-genes$log2FoldChange),], n = top_up_down)
+down_top <- head(genes[order(genes$log2FoldChange),], n = top_up_down)
 
+#pdf(file = "../results/volcano/trt_vs_ctrl.pdf")
+#pdf(file = "../results/volcano/relap_vs_trt.pdf")
+pdf(file = "../results/volcano/relap_vs_ctrl.pdf")
 ggplot(genes, aes(x = log2FoldChange, y = -log10(padj))) + xlab(expression(log[2]("FC"))) +
     ylab(bquote(-log[10](.(pAdjMethod)))) + geom_point(aes(color = Significance)) +
     scale_color_manual(values = c("Sig., abs(log2(FC)) > 1" = "red", "Not signicant" = "grey", "Sig., abs(log2(FC)) < 1" = "blue")) + # remember to change threshold valuse of logFC here for legend
@@ -33,12 +44,14 @@ ggplot(genes, aes(x = log2FoldChange, y = -log10(padj))) + xlab(expression(log[2
     geom_hline(yintercept = -log10(alphaLevel), colour="red", linetype="dashed") + geom_vline(xintercept = logFC_thres, colour="red", linetype="dashed") + geom_vline(xintercept = -logFC_thres, colour="red", linetype="dashed") +
     geom_text_repel(
         #data = subset(genes, padj < geneNamesThres),
-        data = rbind(up_top10, down_top10),
-        aes(label = Gene),
+        data = rbind(up_top, down_top),
+        aes(label = gene),
         size = 5,
         box.padding = unit(0.35, "lines"),
         point.padding = unit(0.3, "lines")
   )
+dev.off()
+
 
 sessionInfo()
 
